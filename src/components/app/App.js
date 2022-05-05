79d772a4cf3d9dcc3e2cb89153dbb258770e19c8
Marc Sanchez <marc.sanchez.civantos@gmail.com>
2022-05-05 23:54:39 +0200

    Architecture

---
+++ src/components/app/App.js
@@ -0,0 +1,12 @@
+import { Wrapper } from "../styled-components/Wrapper";
+
+const App = () => {
+  return (
+    <Wrapper w = "100vw" h = "100vh" justify align >
+      <h1>Titulo</h1>
+      <h1>Titulo</h1>
+    </Wrapper>
+  );
+}
+
+export default App;
